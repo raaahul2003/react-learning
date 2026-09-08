f24c5826e1a7a23e6b8293d0e689867d14b0e088
Rahul Raj C P <rahulrajcp@Rahuls-MacBook-Air.local>
2026-09-08 10:33:45 +0530

    resume builder completed

--- Resume-Builder/resumeBuilder/src/pages/History.jsx
+++ Resume-Builder/resumeBuilder/src/pages/History.jsx
@@ -1,29 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Paper from '@mui/material/Paper';
 import { Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { MdDelete } from "react-icons/md";
-
-
+import { getHistoryApi } from '../services/callApi'
+import {deleteResumeApi} from '../services/callApi'
 
 function History() {
+  const [downloads, setDownloads] = useState({})
+
+  useEffect(() => {
+    getDownloads()
+  }, [])
+
+  const getDownloads = async () => {
+    const response = await getHistoryApi()
+    console.log(response);
+
+    setDownloads(response.data)
+
+  }
+
+  const deleteHistory = (id)=>{
+      deleteResumeApi(id)
+      getDownloads()
+  }
+
   return (
     <div>
-      <h1 className='text-center' style={{marginTop:'100px'}}>Downloaded Resumes</h1>
-      <Link to={'/'} style={{marginTop:'-55px',marginRight:'20px'}} className='float-end btn btn-primary text-light'>Back</Link>
+      <h1 className='text-center' style={{ marginTop: '100px' }}>Downloaded Resumes</h1>
+      <Link to={'/'} style={{ marginTop: '-55px', marginRight: '20px' }} className='float-end btn btn-primary text-light'>Back</Link>
       <Box component="section" className='container-fluid'>
         <div className='row'>
-          <div className='col-md-4'>
-            <Paper elevation={3} sx={{my:5,p:5,textAlign:'center'}}>
-              <div className='d-flex align-items-center justify-content-between'>
-                <h6>Review At: 31/08/2026</h6>
-                <button className='btn text-danger fs-2'><MdDelete /></button>
-              </div>
-              <div>
-                <img src="src/assets/resume.png" alt="" className='img-fluid'/>
-              </div>
-            </Paper>
-          </div>
+          {
+            downloads?.length>0?
+            downloads?.map(dwnld => (
+              <div className='col-md-4' >
+              <Paper elevation={3} sx={{ my: 5, p: 5, textAlign: 'center',}}>
+                <div className='d-flex align-items-center justify-content-between'>
+                  <h6>Review At: {dwnld.timeStamb}</h6>
+                  <button className='btn text-danger fs-2' onClick={()=>deleteHistory(dwnld.id)}><MdDelete /></button>
+                </div>
+                <div>
+                  {/* Preview */}
+                  <Link to={`/resume/${dwnld.resumeId}/view`}>
+                  <img src={dwnld.imgUrl} alt="" className='img-fluid' style={{height: "40vh" }}/>
+                  </Link>
+                </div>
+              </Paper>
+            </div>
+            ))
+            :
+            <p className='text-center text-danger fw-bold my-5'>No Resume Downloaded yet!</p>
+          }
         </div>
       </Box>
     </div>
--- Resume-Builder/resumeBuilder/src/services/callApi.js
+++ Resume-Builder/resumeBuilder/src/services/callApi.js
@@ -8,7 +8,6 @@ export const addResumeApi = async (reqBody) => {
 
 export const getSingleResumeApi = async (id) => {
    console.log(id);
-
    return await apiService("GET", `/allResume/${id}`, {})
 
 }
@@ -17,6 +16,16 @@ export const downloadResumeApi = async (reqBody) => {
    return await apiService("POST", `/historyResume`, reqBody)
 }
 
-export const editResumeApi = async (id,reqBody) => {
+export const editResumeApi = async (id, reqBody) => {
    return await apiService("PUT", `/allResume/${id}`, reqBody)
+}
+
+export const getHistoryApi = async () => {
+   return await apiService("GET", `/historyResume`, {})
+
+}
+
+export const deleteResumeApi = async (id) => {
+   return await apiService("DELETE", `/historyResume/${id}`, {})
+
 }
