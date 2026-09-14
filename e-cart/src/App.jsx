021d2aa7dfa04f8efea13ae3e5141f6d4ada1c02
Rahul Raj C P <rahulrajcp@Rahuls-MacBook-Air.local>
2026-09-14 12:16:28 +0530

    developing a e-cart by redux

---
+++ e-cart/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Route, Routes } from 'react-router'
+import Landing from './pages/Landing'
+import Wishlist from './pages/Wishlist'
+import Cart from './pages/Cart'
+import Product from './pages/Product'
+import Pnf from './pages/Pnf'
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/wishlist' element={<Wishlist />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/product/:id/view' element={<Product />} />
+        <Route path='/pnf' element={<Pnf />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
+
+export default App
