86f9814e1664e90d40591eace498d8d3b3e7c517
Rahul Raj C P <rahulrajcp@Rahuls-MacBook-Air.local>
2026-09-14 10:22:47 +0530

    developed the counter app with redux

--- Redux-CounterApp/src/Components/Counter.jsx
+++ Redux-CounterApp/src/Components/Counter.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, incrementByAmount, reset } from '../redux/counterSlice'
+
 
 function Counter() {
+    const { count } = useSelector(state => state.counter)
+    const dispatch = useDispatch()
+    const valueRef = useRef()
+    
+
+    const handleAddByAmount = () => {
+        let amount = valueRef.current.value
+        if (amount) {
+            dispatch(incrementByAmount(Number(amount)))
+            console.log(amount);
+            
+        }
+        else {
+            alert("Enter a Value!")
+        }
+    }
+
+
+
     return (
         <div className='bg-dark' style={{ height: '100vh' }}>
             <h1 className='text-light text-center p-5'>Counter App</h1>
             <div className='border border-white p-5'>
-                <h1 style={{ fontSize: '50px' }} className='text-center text-white my-5'>0</h1>
+                <h1 style={{ fontSize: '50px' }} className='text-center text-white my-5'>{count}</h1>
                 <div className='d-flex align-items-center justify-content-around'>
-                    <button className='btn btn-success'>INCREMENT[+]</button>
-                    <button className='btn btn-danger'>RESET</button>
-                    <button className='btn btn-warning'>DECREMENT[-]</button>
+                    <button className='btn btn-success' onClick={() => dispatch(increment())}>INCREMENT[+]</button>
+                    <button className='btn btn-danger' onClick={() => dispatch(reset())}>RESET</button>
+                    <button className='btn btn-warning' onClick={() => dispatch(decrement())}>DECREMENT[-]</button>
                 </div>
                 <div className='d-flex align-items-center justify-content-between my-5 p-5'>
-                    <input type="text" placeholder='Enter a value' className='form-control'/>
-                    <button className='btn btn-primary ms-3'>ADD</button>
+                    <input type="text" placeholder='Enter a value' className='form-control' ref={valueRef} />
+                    <button className='btn btn-primary ms-3' onClick={handleAddByAmount}>ADD</button>
                 </div>
             </div>
         </div>
--- Redux-CounterApp/src/redux/counterSlice.js
+++ Redux-CounterApp/src/redux/counterSlice.js
@@ -9,16 +9,23 @@ const counterSlice = createSlice({
         increment: (state, action) => {
             state.count++
         },
-        decrement: (state,action) => {
+        decrement: (state) => {
             state.count--
         },
-        reset: (state,action) => {
-            state.count = 0
+        reset: (state) => {
+            // state.count = 0 //runs because immer library in reduxtoolkit
+            return {...state,count: 0} //old style while using redux
+        },
+        incrementByAmount:(state,action) => {
+            state.count+=action.payload
         }
+        
 
     }
 })
 export const {increment} =counterSlice.actions
 export const {decrement} =counterSlice.actions
 export const {reset} =counterSlice.actions
+export const {incrementByAmount} =counterSlice.actions
+
 export default counterSlice.reducer
